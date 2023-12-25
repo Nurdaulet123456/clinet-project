dcd4ac23ce73f6de67ab8cedf02e20f11ac9690a
Nurdaulet123456 <nurdauletdeveloper@gmail.com>
2023-12-26 02:48:25 +0600

    feat: create new project

--- components/organ/MektepT/MektepT1.tsx
+++ components/organ/MektepT/MektepT1.tsx
@@ -18,7 +18,7 @@ const MektepT1 = () => {
               жырау көшесі, 29 үй{" "}
             </div>
 
-            <div className="mektep_info-content-t">
+            <div className="mektept_info-content_title">
               Мектептің әлеуметтік төлқұжаты
             </div>
 
@@ -61,6 +61,63 @@ const MektepT1 = () => {
             </div>
           </div>
         </div>
+
+        <div className="mektep_p">
+          <div>
+            <div className="mektept_info-content_title">Мектеп төлқұжаты</div>
+            <ul>
+              <li>Оқыту тілі - қазақ, орыс</li>
+              <li>Статусы - тірек мектеп</li>
+              <li>Сыйымдылығы - 314</li>
+              <li>Нақты оқитыны - 812</li>
+            </ul>
+          </div>
+          <div>
+            <div className="mektept_info-content_title">
+              Жалпы сынып-комплект 41
+            </div>
+
+            <ul>
+              <li>Оқыту тілі - қазақ, орыс</li>
+              <li>Статусы - тірек мектеп</li>
+              <li>Сыйымдылығы - 314</li>
+              <li>Нақты оқитыны - 812</li>
+            </ul>
+          </div>
+          <div>
+            <div className="mektept_info-content_title">
+              Педагогтардың жалпы саны - 92
+            </div>
+
+            <ul>
+              <li>Оқыту тілі - қазақ, орыс</li>
+              <li>Статусы - тірек мектеп</li>
+              <li>Сыйымдылығы - 314</li>
+              <li>Нақты оқитыны - 812</li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="mektep_h">
+          <div className="mektept_info-content_title">Мектеп тарихы</div>
+          <p>
+            Қазіргі таңдағы білім ғылымның қарыштап дамуы көз ілестірмей келе
+            жатқан аса ауқымды процесс. Білім дамуының өзгерістері болашақ
+            ұрпақтың жеке тұлға болып қалыптасуына бағытталған. Өзге елдермен
+            иық тіресе алатын, бәсекеге қабілетті бола алатын мемлекет болу үшін
+            білімнің іргесін берік қалау керек. Бұл ретте өскелең ұрпаққа сапалы
+            білім, саналы тәрбие беруде өзіндік қолтаңбасын қалдырып келе жатқан
+            киелі мекеннің төрінен орын алған қасиетті қара шаңырақ №20
+            З.Шүкіров атындағы Бөген орта мектебі екенін айта кету керек. Тарихы
+            әріден бастау алатын білім ұясы – халықтың сан ғасырлар бойы жинаған
+            мәдениетін келешек ұрпаққа жеткізетін ұлттық тәлім - тәрбие
+            ошағы.Аталмыш мектеп 1919 жылы Бөгенде бастауыш мектеп болып ашылды.
+            Мектепті татар байы Темірхан Ярлыгаев өз қаржысымен ашты. Мектепке
+            және алғашқы оқушыларға да керек құрал- жабдықтарды Темірхан әкеліп
+            берді. Оның оқыған, сауатты баласы Атағұла (Атауолла) алғашқы оқу
+            жылындағы жалғыз мұғалім болды.
+          </p>
+        </div>
       </MektepT1Styled>
     </>
   );
@@ -78,12 +135,68 @@ const MektepT1Styled = styled.div`
   .mektept_info-content_title {
     font-size: 1.8rem;
     font-weight: 700;
-    color: #1B447A;
+    color: #1b447a;
     margin-bottom: 1rem;
   }
 
   .mektept_info-content_subtitle {
-    
+    color: #a098ae;
+
+    margin-bottom: 2.4rem;
+  }
+
+  .mektep_info-content_flex {
+    display: flex;
+    flex-wrap: wrap;
+
+    gap: 2rem;
+  }
+
+  .mektep_info-content-t1 {
+    div {
+      &:nth-child(1) {
+        color: #a098ae;
+
+        margin-bottom: 2rem;
+      }
+
+      &:nth-child(2) {
+        display: flex;
+        align-items: center;
+
+        gap: 1rem;
+        color: #303972;
+        font-weight: 700;
+      }
+    }
+  }
+
+  .mektep_p {
+    display: flex;
+    align-items: flex-start;
+
+    justify-content: space-between;
+
+    margin-top: 3.2rem;
+
+    margin-bottom: 2rem;    
+
+    ul {
+      li {
+        display: flex;
+        align-items: center;
+        gap: 1.6rem;
+        margin-bottom: 0.8rem;
+        &::before {
+          content: "";
+          display: block;
+          width: 8px;
+          height: 8px;
+          background-color: #a098ae;
+          border-radius: 50%;
+        }
+      }
+    }
   }
 `;
 
